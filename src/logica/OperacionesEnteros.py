056d5e58b4442a79fe5c4028c5d2aaf217c70983
74174615 <74174615@continental.edu.pe>
2023-04-19 21:11:25 -0500

    mcd de dos numeors

--- src/logica/OperacionesEnteros.py
+++ src/logica/OperacionesEnteros.py
@@ -1,15 +1,5 @@
 class OperacionesEnteros:
-
     def MCD(self, numero1, numero2):
-        temporal = 0
-
-        while numero2 != 0:
-            temporal = numero2
-            numero2 = numero1 % numero2
-            numero1 = temporal
-        return numero1
-
-    def MCD3(self, numero1, numero2, numero3):
-        numero1 = self.MCD(numero1, numero2)
-        numero1=self.MCD(numero1, numero3)
-        return numero1
+        return None
+    def MCD_recursivo(self, numero1, numero2):
+        return None
--- tests/PruebaOperacionesEnteros.py
+++ tests/PruebaOperacionesEnteros.py
@@ -1,33 +1,22 @@
 import unittest
 from src.logica.OperacionesEnteros import OperacionesEnteros
-
-class PruebaOperacionesEnteros(unittest.TestCase):
-    def setUp(self):
-        self.operacion = OperacionesEnteros()
-    def tearDown(self):
-        self.operacion = None
-    def test_MCD_dosNumerosPositivos_retornaMCD(self):
-        # Arrange
-        numero1 = 18
-        numero2 = 24
-        resultadoEsperado = 6
-
-        # Do
-        resultadoActual = self.operacion.MCD(numero1, numero2)
-
-        # Assert
-        self.assertEqual(resultadoEsperado, resultadoActual)
-    def test_MCD_tresNumerosPositivos_retornaMCD(self):
-        # Arrange
-        numero1 = 24679
-        numero2 = 20387
-        numero3 = 16169
-
-        resultadoEsperado = 37
-        # Do
-        resultadoActual = self.operacion.MCD3(numero1, numero2, numero3)
-
-        # Assert
-        self.assertEqual(resultadoEsperado, resultadoActual)
-
-
+    class PruebaOperacionesEnteros(unittest.TestCase):
+        def setUp(self):
+            self.operacion = OperacionesEnteros()
+        def test_MCD_dosNumerosPositivos_retornaMCD(self):
+ # Arrange
+            numero1 = 18
+            numero2 = 24
+            resultadoEsperado = 6 # Do
+            resultadoActual = self.operacion.MCD(numero1, numero2)
+ # Assert
+            self.assertEqual(resultadoEsperado, resultadoActual)
+ def test_MCDRecursivo_dosNumerosPositivos_retornaMCD(self):
+ # Arrange
+    numero1 = 15
+    numero2 = 20
+    resultadoEsperado = 5
+    # Do
+    resultadoActual = self.operacion.MCD_recursivo(numero1, numero2)
+    # Assert
+    self.assertEqual(resultadoEsperado, resultadoActual)
